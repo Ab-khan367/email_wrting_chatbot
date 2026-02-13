fb0b63ea930010db94357eed0e6ad0faa4b747b5
Muhammad Abdullah <139207207+Ab-khan367@users.noreply.github.com>
2026-02-13 00:06:33 -0800

    Update chains.py

--- app/backend/chains.py
+++ app/backend/chains.py
@@ -1,7 +1,6 @@
 
 import os
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
